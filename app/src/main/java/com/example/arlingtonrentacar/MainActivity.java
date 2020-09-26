70481f8f5d792c4def1919de12b104b1153645a6
ashwinigit123 <ashwinitrale8@gmail.com>
2020-09-26 14:56:51 +0530

    Added manager home screen and DB

--- app/src/main/java/com/example/arlingtonrentacar/MainActivity.java
+++ app/src/main/java/com/example/arlingtonrentacar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.arlingtonrentacar;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,18 +9,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editText_username, editText_password;
     String username, password;
     Intent intent;
+    //ashwini
+    UserDao mydb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //ashwini
+        mydb = new UserDao(this);
+        addRecordsToDb();
     }
 
     public void checkValidUser(View view){
@@ -38,10 +45,17 @@ public class MainActivity extends AppCompatActivity {
 //          if they match start an activity according to the role
             else{
 //                if(role.trim().toLowerCase() == "user"){
-                    startActivity(new Intent(this, testactivity.class));
+                    //ashwini: redirected to manager home screen for testing
+                    startActivity(new Intent(this, ManagerHomeScreen.class));
 //                }
             }
         }
 
+    }
+    //ashwini
+    public void addRecordsToDb(){
+        String rec1= mydb.addRecord("Prajakta","Waikar","prajuw","prajPass1","17282","Renter","prajakta1@gmail.com","9877545409","str2","pune","MH","62525","yes");
+        String rec2= mydb.addRecord("Ashwini","Trale","ashut","ashuPass2","17283","Manager","ashwinit6@gmail.com","9877006745","str1","pune","MH","62585","no");
+        String rec3= mydb.addRecord("Shubham","Patil","shubhp","shubhPass3","17284","Admin","shubhamp123@gmail.com","9187092454","str3","pune","MH","60585","no");
     }
 }
---
+++ app/src/main/java/com/example/arlingtonrentacar/ManagerHomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.arlingtonrentacar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home_screen);
+    }
+}
