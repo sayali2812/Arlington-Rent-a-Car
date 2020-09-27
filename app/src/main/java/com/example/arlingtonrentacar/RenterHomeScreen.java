44fb71f2fa254b3c92e8a335fdbe3f99ac053777
Sayali2812 <sayali.deshmukh@mavs.uta.edu>
2020-09-28 01:33:33 +0530

    Addeds Renret Home Screen and Admin Home Screen

---
+++ app/src/main/java/com/example/arlingtonrentacar/RenterHomeScreen.java
@@ -0,0 +1,14 @@
+package com.example.arlingtonrentacar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RenterHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_renter_home_screen);
+    }
+}
